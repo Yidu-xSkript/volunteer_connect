8f3839f162f66b61a26d0e189c978725d9eede37
Yidu-xSkript <yidu95@gmail.com>
2023-03-08 14:51:27 +0300

    input component + majority of auth design (layout) complete

--- frontend/src/components/form/input.js
+++ frontend/src/components/form/input.js
@@ -1,5 +1,11 @@
-const input = () => (
-    <></>
+const input = ({ type="text", placeholder, icon, required=false }) => (
+    <div className="relative w-2/3">
+        {/* <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div> */}
+        <input className="rounded-full h-14 my-3 text-lg duration-150 focus:bg-white focus:shadow-md focus:outline-none focus:ring focus:ring-[#1C7280] w-full p-5 bg-[#F5F9F9]" type={type} placeholder={placeholder} required={required} />
+        {icon && <ic>{icon}</ic>}
+    </div>
 );
 
 export default input;
--- frontend/src/pages/signup/index.js
+++ frontend/src/pages/signup/index.js
@@ -2,32 +2,50 @@
 import afr from '../../assets/afr-vc.svg'
 import authCircle from '../../assets/vc-auth-circle-1.svg'
 import authCircleWhite from '../../assets/auth-circle-white.svg'
-const signup = () => (
-  <>
-    <div className='flex z-0'>
-      <div className='bg-[#E3EFF1] w-1/2 h-full absolute top-0 bottom-0 p-16'>
-        {/* <img src={logo} className="w-40" alt="logo" /> */}
-        <img src={authCircle} className="w-64 z-0 absolute -top-20 -left-20 pointer-events-none select-none" alt="circle-bg" />
-        <img src={authCircle} className="w-[26rem] z-0 absolute -top-36 right-0 pointer-events-none select-none" alt="circle-bg" />
-        <img src={authCircle} className="w-[30rem] z-0 absolute bottom-0 -left-32 pointer-events-none select-none" alt="circle-bg" />
-        <img src={authCircleWhite} className="w-[36rem] z-0 absolute bottom-0 -right-32 pointer-events-none select-none" alt="circle-bg" />
-        <h1 className="text-6xl font-bold z-10 relative">Logo</h1>
-        <div className='mt-20'>
-          <h1 className='text-[#2D9CAE] font-heading text-6xl tracking-wide'>Welcome</h1>
-          <h1 className='text-gray-800 font-heading text-6xl tracking-wide mt-5'>To Volunteer Connect</h1>
-        </div>
-        <img src={afr} className="w-[85%] absolute bottom-0 pointer-events-none select-none" alt="signup-vc-img" />
-      </div>
-      <div className='w-1/2 h-full'></div>
-      <div className='w-1/2 h-full p-16'>
-        <p className='text-gray-400 text-right'>Already a member? <a href={`/login`} className="font-semibold text-gray-800">Sign in</a></p>
-        <div className="mt-20">
-          <h1 className="font-heading text-5xl text-center">Sign up to connect</h1>
-          {/* Input */}
-        </div>
-      </div>
-    </div>
-  </>
-);
+import { Link } from "react-router-dom";
+import Input from '../../components/form/input';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
-export default signup;
+function Signup() {
+  const [passwordVisibility, setPasswordVisibility] = useState("password")
+  const handlePasswordVisibility = () => {
+    setPasswordVisibility(passwordVisibility === "password" ? "text" : "password")
+  }
+  return (
+    <>
+      <div className='flex z-0'>
+        <div className='bg-[#E3EFF1] w-1/2 h-full absolute top-0 bottom-0 p-16'>
+          {/* <img src={logo} className="w-40" alt="logo" /> */}
+          <img src={authCircle} className="w-64 z-0 absolute -top-20 -left-20 pointer-events-none select-none" alt="circle-bg" />
+          <img src={authCircle} className="w-[26rem] z-0 absolute -top-36 right-0 pointer-events-none select-none" alt="circle-bg" />
+          <img src={authCircle} className="w-[30rem] z-0 absolute bottom-0 -left-32 pointer-events-none select-none" alt="circle-bg" />
+          <img src={authCircleWhite} className="w-[36rem] z-0 absolute bottom-0 -right-32 pointer-events-none select-none" alt="circle-bg" />
+          <h1 className="text-5xl font-bold z-10 relative">Logo</h1>
+          <div className='mt-20'>
+            <h1 className='text-[#1C7280] font-heading text-6xl tracking-wide'>Welcome</h1>
+            <h1 className='text-gray-800 font-heading text-6xl tracking-wide mt-5'>To Volunteer Connect</h1>
+          </div>
+          <img src={afr} className="w-[85%] absolute bottom-0 pointer-events-none select-none" alt="signup-vc-img" />
+        </div>
+        <div className='w-1/2 h-full'></div>
+        <div className='w-1/2 h-full p-16'>
+          <p className='text-gray-400 text-right'>Already a member? <Link to='/login' className="font-semibold text-gray-800">Sign in</Link></p>
+          <div className="mt-20 flex flex-col items-center justify-center">
+            <h1 className="font-heading text-5xl text-center mb-20">Sign up to connect</h1>
+            <Input type={"text"} placeholder={"Name"} />
+            <Input type={"email"} placeholder={"Email"} />
+            <Input type={passwordVisibility} placeholder={"Password"} icon={
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={handlePasswordVisibility}>
+                {passwordVisibility === "password" ?<EyeIcon className='w-6 hover:text-gray-700 text-gray-400 duration-150' /> : <EyeSlashIcon className='w-6 hover:text-gray-700 text-gray-400 duration-150' />}
+              </div>
+            } />
+            {/* <Input type={"password"} placeholder={"Password"} /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
