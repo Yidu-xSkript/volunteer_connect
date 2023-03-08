33a092e495ac4b3b757d86f56a5783769c4b7fd3
Yidu-xSkript <yidu95@gmail.com>
2023-03-08 15:41:25 +0300

    dropdown component outline fix

--- frontend/src/components/form/dropdown.js
+++ frontend/src/components/form/dropdown.js
@@ -1,7 +1,7 @@
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
 const dropdown = ({ options, required=false }) => (
-    <div className='relative rounded-full h-14 my-3 focus:bg-white focus:shadow-md  focus:outline-none focus:ring focus:ring-[#1C7280] w-2/3 px-5 bg-[#F5F9F9]'>
-        <select className="rounded-full h-14 bg-transparent w-full text-lg duration-150 appearance-none" required={required}>
+    <div className='relative rounded-full h-14 my-3 focus:bg-white focus:shadow-md focus:ring focus:ring-[#1C7280] w-2/3 px-5 bg-[#F5F9F9]'>
+        <select className="rounded-full h-14 bg-transparent w-full text-lg duration-150 focus:outline-none appearance-none" required={required}>
             {options.map((op) => <option className='appearance-none' value={op.id} selected={op.id === 1}>{op.name}</option>)}
         </select>
         <div className="absolute inset-y-0 right-0 flex items-center pr-3 z-10">
