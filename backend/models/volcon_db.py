a6e88dd7b0e2721139919daa8d444ad751836f0e
DMMutua <lengowriter99@gmail.com>
2023-03-09 11:39:02 +0300

    defining ORM Classes for missions, applications and volunteers tables

--- backend/models/volcon_db.py
+++ backend/models/volcon_db.py
@@ -27,3 +27,50 @@ class User(db.Model):
         'polymorphic_on': role
     }
 
+class Organization(User):
+    """Defines the Organization Table; which is a Child Table of the User Table"""
+
+    __tablename__ = 'organizations'
+
+    org_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    biography = db.Column(db.Text, nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'organization'
+    }
+
+class Mission(db.Model):
+    """Defines the Mission Table; Which is a Child Table for the User Table
+    `Missions` table has a 1:Many Relationship with the Application table"""
+
+    __tablename__ = 'missions'
+
+    mission_id = db.Column(db.Integer, primary_key=True)
+    org_id = db.Column(db.Integer, db.ForeignKey('organizations.org_id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+    max_people = db.Column(db.Integer, nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+
+class Application(db.Model):
+    """Defines the Application Table; 
+    which has a 1:1 relationship with the `Missions` Table;
+    """
+
+    __tablename__ = 'applications'
+
+    application_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('volunteers.user_id'), nullable=False)
+    mission_id = db.Column(db.Integer, db.ForeignKey('missions.mission_id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    status = db.Column(db.Boolean, nullable=True)
+
+    mission = db.relationship('Mission', backref='applications')
+    volunteer = db.relationship('Volunteer', backref='applications')
