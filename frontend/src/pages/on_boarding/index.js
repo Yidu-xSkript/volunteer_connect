0936a0fb23a98407a773dac2cc9fd305a7b771b1
Yidu-xSkript <yidu95@gmail.com>
2023-03-09 08:46:16 +0300

    On-boarding scaffolding fix

--- frontend/src/pages/on_boarding/index.js
+++ frontend/src/pages/on_boarding/index.js
@@ -21,28 +21,28 @@ function OnBoarding() {
   return (
     <div className="flex">
       {/* Sidebar */}
-      <div className="w-1/3 bg-auth h-full absolute top-0 bottom-0 block">
+      <div className="w-1/4 bg-auth h-full absolute top-0 bottom-0 block">
         <img src={authCircle} className="w-64 z-0 absolute -top-20 -left-20 pointer-events-none select-none" alt="circle-bg" />
-        <img src={authCircle} className="w-96 z-0 absolute -top-36 right-0 pointer-events-none select-none" alt="circle-bg" />
+        <img src={authCircle} className="w-80 z-0 absolute -top-20 right-0 pointer-events-none select-none" alt="circle-bg" />
         <div className="p-16">
           {/* <img src={logo} className="w-40 relative z-10" alt="logo" /> */}
-          <h1 className="text-5xl font-bold z-10 relative mb-32">Logo</h1>
+          <h1 className="text-5xl text-black font-bold z-10 relative mb-32">Logo</h1>
           {wizards.map((w, i) => (
             <div key={i} className="flex items-start space-x-4 my-2">
               <div className="flex flex-col items-center">
-                <span className={`h-8 w-8 rounded-full border border-black ${selectedPill === i ? 'bg-primary' : 'bg-transparent'}`} />
-                {i < wizards.length - 1 && <span className="border h-10 border-black mt-2" />}
+                <span className={`h-8 w-8 rounded-full border border-black ${selectedPill >= i ? 'bg-primary' : 'bg-transparent'}`} />
+                {i < wizards.length - 1 && <span className={`border h-10 mt-2 ${selectedPill > i ? 'border-primary' : 'border-black'}`} />}
               </div>
-              <h3 className={`text-2xl select-none font-semibold ${selectedPill === i ? 'text-black' : 'text-gray-400'}`}>{w}</h3>
+              <h3 className={`text-2xl select-none z-10 font-semibold ${selectedPill >= i ? 'text-black' : 'text-gray-400'}`}>{w}</h3>
             </div>
           ))}
         </div>
-        <div className="absolute select-none bottom-10 left-0 p-8 shadow bg-white border border-gray-100 uppercase tracking-[0.5rem] font-semibold text-3xl rounded-tr-full rounded-br-full">
+        <div className="absolute select-none bottom-10 left-0 p-8 shadow bg-white border border-gray-100 uppercase tracking-[0.4rem] font-semibold text-2xl rounded-tr-full rounded-br-full">
           {wizards[selectedPill]}
         </div>
       </div>
-      <div className='w-1/3 h-full' />
-      <div className="w-2/3 p-16">
+      <div className='w-1/4 h-full' />
+      <div className="w-3/4 p-16">
         {/* Nav */}
         <div className="flex justify-between items-center">
           <ChevronLeftIcon className="w-16 p-5 hover:bg-[#F5F9F9] rounded-full cursor-pointer" onClick={decrementPill}/>
@@ -54,7 +54,7 @@ function OnBoarding() {
         <div className="flex flex-col items-center">
           {selectedPill === 0 &&
             <>
-              <img src={welcome} className="w-2/3" alt="Welcome" />
+              <img src={welcome} className="w-2/4" alt="Welcome" />
               <h2 className="border-b-2 border-black font-semibold text-3xl pb-4 tracking-wide">Glad You're here, name!</h2>
               <h1 className="font-heading text-black text-5xl mt-5">Thank you <span className="text-primary">for donating your time.</span></h1>
             </>
@@ -71,7 +71,7 @@ function OnBoarding() {
 
             </>
           }
-          <FormButton text={selectedPill === wizards.length - 1 ? "Finish" : selectedPill === 0 ? "Get Started" : "Next"} className={"rounded-full mt-10 w-1/2"} action={incrementPill} />
+          <FormButton text={selectedPill === wizards.length - 1 ? "Submit" : selectedPill === 0 ? "Get Started" : "Next"} className={"rounded-full mt-10 w-1/2"} action={incrementPill} />
         </div>
       </div>
     </div>
