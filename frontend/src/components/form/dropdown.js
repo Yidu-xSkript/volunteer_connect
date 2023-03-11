988cf68ac7986566ecac0a0fcfe7f00353e773ba
Yidu-xSkript <yidu95@gmail.com>
2023-03-11 08:30:21 +0300

    Dropdown class, icon option & input Icon options updated | Signup & login refactor

--- frontend/src/components/form/dropdown.js
+++ frontend/src/components/form/dropdown.js
@@ -1,6 +1,7 @@
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
-const dropdown = ({ options, required=false }) => (
-    <div className='relative rounded-full h-14 my-3 focus:bg-white focus:shadow-md focus:ring focus:ring-[#1C7280] w-2/3 px-5 bg-[#F5F9F9]'>
+const dropdown = ({ options, className, icon, required=false }) => (
+    <div className={`${className} relative rounded-full h-14 my-3 focus:bg-white focus:shadow-md focus:ring focus:ring-[#1C7280] w-2/3 px-5`}>
+        {icon && <span>{icon}</span>}
         <select className="rounded-full h-14 bg-transparent w-full text-lg duration-150 focus:outline-none appearance-none" required={required}>
             {options.map((op) => <option key={op.id} className='appearance-none' value={op.id} defaultValue={op.id === 1}>{op.name}</option>)}
         </select>
