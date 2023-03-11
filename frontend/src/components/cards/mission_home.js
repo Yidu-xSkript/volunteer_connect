0934b866b08bdad7d724ab8e21a7b7f2942dc36f
Yidu-xSkript <yidu95@gmail.com>
2023-03-11 08:33:19 +0300

    onclick event added

--- frontend/src/components/cards/mission_home.js
+++ frontend/src/components/cards/mission_home.js
@@ -1,7 +1,7 @@
 import { MapPinIcon } from "@heroicons/react/24/solid";
 
 const MissionHomeCard = ({image, title, post_human_date, volunteering_hours, est_time, deadline, volunteer_amount, description, location, action}) => (
-    <div onClick={action} className="cursor-pointer rounded-2xl bg-[#fbfbfb] border border-gray-200 space-y-4 shadow shadow-slate-50 p-6">
+    <div onClick={action} className="cursor-pointer rounded-2xl bg-[#fbfbfb] border border-gray-200 space-y-4 shadow shadow-slate-50 p-6 hover:shadow-lg hover:border-gray-300 duration-300">
         <div className="flex items-center space-x-7">
             <div className="border w-24 rounded-2xl border-gray-300">
                 <img src={image} alt={title} />
