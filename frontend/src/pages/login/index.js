8c0f9e2e3c3c391c47f67ac250f75eb884844788
Yidu-xSkript <yidu95@gmail.com>
2023-03-11 08:30:43 +0300

    Dropdown class, icon option & input Icon options updated | Signup & login refactor

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -14,7 +14,7 @@ function Login() {
     <GuestLayout>
       <Input type={"email"} placeholder={"Email"} required={true} uClass='w-2/3' className={'bg-[#F5F9F9]'} />
       <Input type={passwordVisibility} placeholder={"Password"} icon={
-        <div className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={handlePasswordVisibility}>
+        <div onClick={handlePasswordVisibility}>
           {passwordVisibility === "password" ? <EyeIcon className='w-6 hover:text-gray-700 text-gray-400 duration-150' /> : <EyeSlashIcon className='w-6 hover:text-gray-700 text-gray-400 duration-150' />}
         </div>
       } required={true} uClass='w-2/3' className={'bg-[#F5F9F9]'} />
