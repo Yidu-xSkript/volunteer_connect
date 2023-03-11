da7b3c3f2125d38edd76133802c41a0905351167
Yidu-xSkript <yidu95@gmail.com>
2023-03-11 10:56:29 +0300

    Checkbox added to filter form

--- frontend/src/components/form/filter.js
+++ frontend/src/components/form/filter.js
@@ -1,5 +1,6 @@
-import { XCircleIcon, MapPinIcon } from "@heroicons/react/24/outline";
+import { XCircleIcon, MapPinIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import Dropdown from "./dropdown";
+import CheckBox from "./checkbox";
 
 function Filter() {
     const locations = [
@@ -22,12 +23,13 @@ function Filter() {
         {id:4, name: 'More than 100'},
     ]
     return (
-        <div className="shadow shadow-slate-50 rounded-2xl p-5 border border-gray-200 space-y-10">
+        <div className="shadow shadow-slate-50 rounded-2xl p-5 border border-gray-200 space-y-5">
             {/* Title */}
             <div className="flex justify-between items-center">
                 <h3 className="text-xl font-semibold tracking-wide">Filter</h3>
                 <div className="text-base text-gray-400 flex items-end space-x-1 cursor-pointer"><span>Clear all</span> <XCircleIcon className="w-6" /> </div>
             </div>
+            <hr class="h-px bg-gray-100 border-0 w-full"/>
             {/* Location Filter -- populate data from an API */}
             <div className="">
                 <label className="font-semibold">Location</label>
@@ -37,25 +39,47 @@ function Filter() {
                     </div>
                 } />
             </div>
+            <hr class="h-px bg-gray-100 border-0 w-full"/>
             {/* Category Filter */}
-            <div className="">
+            <div className="space-y-2">
                 <label className="font-semibold">Category</label>
-
+                <div className="space-y-1">
+                    <CheckBox name={"Category 1"} value={"Category1"} />
+                    <CheckBox name={"Category 2"} value={"Category2"} />
+                    <CheckBox name={"Category 3"} value={"Category3"} />
+                    <CheckBox name={"Category 4"} value={"Category4"} />
+                    <CheckBox name={"Category 5"} value={"Category5"} />
+                </div>
+                <ChevronDownIcon className='w-6 cursor-pointer' title="Show More Categories" />
             </div>
+            <hr class="h-px bg-gray-100 border-0 w-full"/>
             {/* Organization Filter */}
-            <div className="">
+            <div className="space-y-2">
                 <label className="font-semibold">Organizations</label>
-
+                <div className="space-y-1">
+                    <CheckBox name={"Organization 1"} value={"Organization1"} />
+                    <CheckBox name={"Organization 2"} value={"Organization2"} />
+                    <CheckBox name={"Organization 3"} value={"Organization3"} />
+                    <CheckBox name={"Organization 4"} value={"Organization4"} />
+                    <CheckBox name={"Organization 5"} value={"Organization5"} />
+                </div>
+                <ChevronDownIcon className='w-6 cursor-pointer' title="Show More Organizations" />
             </div>
+            <hr class="h-px bg-gray-100 border-0 w-full"/>
             {/* Applicants Filter */}
             <div className="">
                 <label className="font-semibold">Applicants</label>
                 <Dropdown options={applicants} selectClass={'text-gray-400'} className={'w-full shadow shadow-slate-200 border border-gray-100 rounded-xl'} />
             </div>
+            <hr class="h-px bg-gray-100 border-0 w-full"/>
             {/* Work Experience Filter */}
-            <div className="">
+            <div className="space-y-2">
                 <label className="font-semibold">Experience</label>
-
+                <div className="space-y-1">
+                    <CheckBox name={"Experience 1"} value={"Experience1"} />
+                    <CheckBox name={"Experience 2"} value={"Experience2"} />
+                </div>
+                <ChevronDownIcon className='w-6 cursor-pointer' title="Show More Experiences" />
             </div>
         </div>
     );
