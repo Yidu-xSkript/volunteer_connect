4f81aafa35e22ce21988bd3129b7b2258cc2626f
Yidu-xSkript <yidu95@gmail.com>
2023-03-13 10:52:22 +0300

    minor design fixes on homepages mission cards

--- frontend/src/components/cards/mission_home.js
+++ frontend/src/components/cards/mission_home.js
@@ -7,8 +7,9 @@ const MissionHomeCard = ({image, title, post_human_date, volunteering_hours, est
                 <img src={image} alt={title} />
             </div>
             <div className="space-y-3">
-                <div className="flex space-x-3 items-end">
-                    <h1 className="text-2xl font-bold">{title} .</h1>
+                <div className="flex space-x-3 items-center">
+                    <h1 className="text-2xl font-bold">{title}</h1>
+                    <p>•</p>
                     <p className="text-sm text-gray-500">{post_human_date}</p>
                 </div>
                 <p className="text-xs text-gray-500">Volunteering Hours: {volunteering_hours} - Est. Time: {est_time} - Application Deadline: {deadline} - {volunteer_amount} Volunteers Needed</p>
