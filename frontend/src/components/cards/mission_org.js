f78c541ece296b9b1703ec3ee82e29fc95d7fcb3
Yidu-xSkript <yidu95@gmail.com>
2023-03-13 16:43:13 +0300

    Created Organization mission card component | added mission card into missions page and populated it with fake static data

---
+++ frontend/src/components/cards/mission_org.js
@@ -0,0 +1,31 @@
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+function OrgMissionCard({id, title, post_human_date, description, deadline, volunteering_hours, est_time, applicants}) {
+    // Use ID for edit & delete modal.
+
+    const openEditModal = () => {}
+    const openDeleteModal = () => {}
+
+    return (
+        <div className="rounded-3xl border border-gray-300 shadow-lg shadow-slate-100 p-10 bg-white space-y-5">
+            <div className="flex items-start justify-between">
+                <div>
+                    <h2 className="font-bold text-2xl">{title}</h2>
+                    <small className="text-xs text-gray-400">{post_human_date}</small>
+                </div>
+                <div className="flex space-x-2">
+                    <PencilSquareIcon title="Edit Mission" className="w-11 rounded-full shadow p-2 border border-gray-300 bg-gray-100 cursor-pointer" onClick={openEditModal} />
+                    <TrashIcon title="Delete Mission" className="w-11 rounded-full shadow p-2 border border-gray-300 bg-gray-100 cursor-pointer" onClick={openDeleteModal} />
+                </div>
+            </div>
+            <p>{description.substring(0, 300)}...</p>
+            <div>
+                <p className="text-xs text-gray-400">Application Deadline: {deadline}</p>
+                <p className="text-xs text-gray-400">Volunteering Hours: {volunteering_hours} {volunteering_hours > 1 ? 'hrs' : 'hr'}/day</p>
+                <p className="text-xs text-gray-400">Est. Time: {est_time}</p>
+                <p className="text-xs text-gray-400">Applicants: {applicants}</p>
+            </div>
+        </div>
+    );
+}
+
+export default OrgMissionCard;
--- frontend/src/pages/missions/index.js
+++ frontend/src/pages/missions/index.js
@@ -1,6 +1,97 @@
+import OrgMissionCard from "../../components/cards/mission_org";
+import AuthLayout from "../../Layout/Auth";
+import { PlusIcon } from "@heroicons/react/24/outline";
+
 function Mission() {
+    const missions = [
+        {
+            id: 1,
+            applicants: 5,
+            deadline: 'Feb 02, 23',
+            est_time: '1 to 3 months',
+            post_human_date: '2 hours ago',
+            title: 'Mission Title 1',
+            volunteering_hours: 5,
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        },
+        {
+            id: 2,
+            applicants: 5,
+            deadline: 'Feb 02, 23',
+            est_time: '1 to 3 months',
+            post_human_date: '2 hours ago',
+            title: 'Mission Title 1',
+            volunteering_hours: 5,
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        },
+        {
+            id: 3,
+            applicants: 5,
+            deadline: 'Feb 02, 23',
+            est_time: '1 to 3 months',
+            post_human_date: '2 hours ago',
+            title: 'Mission Title 1',
+            volunteering_hours: 5,
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        },
+        {
+            id: 4,
+            applicants: 5,
+            deadline: 'Feb 02, 23',
+            est_time: '1 to 3 months',
+            post_human_date: '2 hours ago',
+            title: 'Mission Title 1',
+            volunteering_hours: 5,
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        },
+        {
+            id: 5,
+            applicants: 5,
+            deadline: 'Feb 02, 23',
+            est_time: '1 to 3 months',
+            post_human_date: '2 hours ago',
+            title: 'Mission Title 1',
+            volunteering_hours: 5,
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        },
+        {
+            id: 6,
+            applicants: 5,
+            deadline: 'Feb 02, 23',
+            est_time: '1 to 3 months',
+            post_human_date: '2 hours ago',
+            title: 'Mission Title 1',
+            volunteering_hours: 5,
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        }
+    ]
     return (
-        <></>
+        <AuthLayout>
+            <div className="relative">
+                <div className="p-10 mx-auto container">
+                    <div className="flex justify-center my-10">
+                        <h1 className="text-4xl uppercase tracking-[0.3rem] font-light">Missions</h1>
+                    </div>
+                    <div className="grid grid-cols-3 gap-7 my-10">
+                        {missions.map((mission) => (
+                            // I'm referring to the attributes 1by1 for ease. I'm gonna pass the whole object later when i send a get request to the api.
+                            <OrgMissionCard
+                                key={mission.id}
+                                applicants={mission.applicants}
+                                deadline={mission.deadline}
+                                description={mission.description}
+                                est_time={mission.est_time}
+                                id={mission.id}
+                                post_human_date={mission.post_human_date}
+                                title={mission.title}
+                                volunteering_hours={mission.volunteering_hours} />
+                        ))}
+                    </div>
+                    <div className="cursor-pointer text-lg text-center uppercase tracking-[0.2rem] font-semibold" title="Load More Missions">Load More</div>
+                </div>
+                <PlusIcon className="w-20 p-3 rounded-full bg-primary shadow-xl absolute bottom-10 right-10 text-white cursor-pointer" title="Add Mission" />
+            </div>
+        </AuthLayout>
     );
 }
 export default Mission;
