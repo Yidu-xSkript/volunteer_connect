196042188b62953bf342f82f4ec7cec96d0424f4
Yidu-xSkript <yidu95@gmail.com>
2023-03-13 18:03:58 +0300

    added the empty illustration | updated missions page => check if there is any content otherwise show empty message.

--- frontend/src/pages/missions/index.js
+++ frontend/src/pages/missions/index.js
@@ -1,6 +1,8 @@
 import OrgMissionCard from "../../components/cards/mission_org";
 import AuthLayout from "../../Layout/Auth";
 import { PlusIcon } from "@heroicons/react/24/outline";
+import Button from "../../components/form/button";
+import empty from "../../assets/empty.svg";
 
 function Mission() {
     const missions = [
@@ -69,27 +71,37 @@ function Mission() {
         <AuthLayout>
             <div className="relative">
                 <div className="p-10 mx-auto container">
-                    <div className="flex justify-center my-10">
+                    {missions.length > 0 && <div className="flex justify-center my-10">
                         <h1 className="text-4xl uppercase tracking-[0.3rem] font-light">Missions</h1>
-                    </div>
-                    <div className="grid grid-cols-3 gap-7 my-10">
-                        {missions.map((mission) => (
-                            // I'm referring to the attributes 1by1 for ease. I'm gonna pass the whole object later when i send a get request to the api.
-                            <OrgMissionCard
-                                key={mission.id}
-                                applicants={mission.applicants}
-                                deadline={mission.deadline}
-                                description={mission.description}
-                                est_time={mission.est_time}
-                                id={mission.id}
-                                post_human_date={mission.post_human_date}
-                                title={mission.title}
-                                volunteering_hours={mission.volunteering_hours} />
-                        ))}
-                    </div>
-                    <div className="cursor-pointer text-lg text-center uppercase tracking-[0.2rem] font-semibold" title="Load More Missions">Load More</div>
+                    </div>}
+                    {/* Checks if there are any missions created */}
+                    {missions.length > 0 ?
+                        <div className="grid grid-cols-3 gap-7 my-10">
+                            {missions.map((mission) => (
+                                // I'm referring to the attributes 1by1 for ease. I'm gonna pass the whole object later when i send a get request to the api.
+                                <OrgMissionCard
+                                    key={mission.id}
+                                    applicants={mission.applicants}
+                                    deadline={mission.deadline}
+                                    description={mission.description}
+                                    est_time={mission.est_time}
+                                    id={mission.id}
+                                    post_human_date={mission.post_human_date}
+                                    title={mission.title}
+                                    volunteering_hours={mission.volunteering_hours} />
+                            ))}
+                        </div> :
+                        <div className="rounded-3xl border border-gray-300 shadow p-10 flex items-center">
+                            <img src={empty} alt="No Content Found."  className="w-1/4" />
+                            <div className="flex flex-col items-center justify-center w-3/4 space-y-6">
+                                <h3 className="uppercase tracking-[0.6rem] font-light text-4xl">Empty!</h3>
+                                <p className="text-2xl font-light text-gray-500 capitalize">You have not created any missions yet!</p>
+                                <Button text={"Create Mission"} className="text-xl px-8 rounded-full" />
+                            </div>
+                        </div>}
+                    {missions.length > 0 && <div className="cursor-pointer text-lg text-center uppercase tracking-[0.2rem] font-semibold" title="Load More Missions">Load More</div>}
                 </div>
-                <PlusIcon className="w-20 p-3 rounded-full bg-primary shadow-xl absolute bottom-10 right-10 text-white cursor-pointer" title="Add Mission" />
+                <PlusIcon className="w-20 p-3 rounded-full bg-primary shadow-xl fixed bottom-10 right-10 text-white cursor-pointer" title="Add Mission" />
             </div>
         </AuthLayout>
     );
