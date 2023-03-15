3464bde668b0e416ac20c3bd2572cc4debaa160f
DMMutua <lengowriter99@gmail.com>
2023-03-15 13:11:14 +0300

    adding API CRUD ops for the mission objects

---
+++ backend/volcon/missions/mission_cruds.py
@@ -0,0 +1,89 @@
+from flask import jsonify, request
+from sqlalchemy.exc import SQLAlchemyError
+from volcon_db import db, Mission
+
+class mission_cruds:
+    @staticmethod
+    def create_mission(org_id):
+        try:
+            data = request.get_json()
+            mission = Mission(
+                org_id=org_id,
+                name=data['name'],
+                description=data['description'],
+                location=data['location'],
+                start_date=data['start_date'],
+                end_date=data['end_date']
+            )
+            db.session.add(mission)
+            db.session.commit()
+            return jsonify({'message': 'Mission created successfully'})
+        except SQLAlchemyError as e:
+            error = str(e.__dict__.get('orig', e))
+            return jsonify({'error': error}), 500
+
+    @staticmethod
+    def get_all_missions():
+        try:
+            missions = Mission.query.all()
+            return jsonify([mission.serialize() for mission in missions])
+        except SQLAlchemyError as e:
+            error = str(e.__dict__.get('orig', e))
+            return jsonify({'error': error}), 500
+
+    @staticmethod
+    def get_mission_by_id(mission_id):
+        try:
+            mission = Mission.query.filter_by(id=mission_id).first()
+            if mission:
+                return jsonify(mission.serialize())
+            else:
+                return jsonify({'message': 'Mission not found'})
+        except SQLAlchemyError as e:
+            error = str(e.__dict__.get('orig', e))
+            return jsonify({'error': error}), 500
+
+    @staticmethod
+    def update_mission(mission_id):
+        try:
+            data = request.get_json()
+            mission = Mission.query.filter_by(id=mission_id).first()
+            if mission:
+                mission.name = data.get('name', mission.name)
+                mission.description = data.get('description', mission.description)
+                mission.location = data.get('location', mission.location)
+                mission.start_date = data.get('start_date', mission.start_date)
+                mission.end_date = data.get('end_date', mission.end_date)
+                db.session.commit()
+                return jsonify({'message': 'Mission updated successfully'})
+            else:
+                return jsonify({'message': 'Mission not found'})
+        except SQLAlchemyError as e:
+            error = str(e.__dict__.get('orig', e))
+            return jsonify({'error': error}), 500
+
+    @staticmethod
+    def delete_mission(mission_id):
+        try:
+            mission = Mission.query.filter_by(id=mission_id).first()
+            if mission:
+                db.session.delete(mission)
+                db.session.commit()
+                return jsonify({'message': 'Mission deleted successfully'})
+            else:
+                return jsonify({'message': 'Mission not found'})
+        except SQLAlchemyError as e:
+            error = str(e.__dict__.get('orig', e))
+            return jsonify({'error': error}), 500
+
+        @staticmethod
+        def serialize(self):
+            return {
+                'id': self.id,
+                'org_id': self.org_id,
+                'name': self.name,
+                'description': self.description,
+                'location': self.location,
+                'start_date': self.start_date.isoformat(),
+                'end_date': self.end_date.isoformat()
+            }
