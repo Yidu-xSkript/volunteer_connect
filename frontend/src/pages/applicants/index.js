a7064b19bca7aa0690bf37ee1c887ddc0bec10dd
Yidu-xSkript <yidu95@gmail.com>
2023-03-19 20:04:53 +0300

    Applicants responsive

--- frontend/src/pages/applicants/index.js
+++ frontend/src/pages/applicants/index.js
@@ -39,6 +39,17 @@ function Applicants() {
             mission_name: 'Mission Title Another and longer title is title',
             approval_status: NaN
         },
+        {
+            id: 4,
+            name: 'Christopher Wilson',
+            bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            email: 'yidu95@gmail.com',
+            image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+            phone_number: '+251932382336',
+            resume: 'https://www.oanda.com/assets/documents/1263/OGM_customer_bonus_promotion_08.07.2022.pdf',
+            mission_name: 'Mission 1',
+            approval_status: 1
+        },
     ]
 
     function handleApprovalStatus(state) {
@@ -47,12 +58,12 @@ function Applicants() {
 
     return (
         <AuthLayout>
-            <div className="flex p-20 space-x-7">
-                <div className="w-1/4 border border-gray-300 shadow rounded-3xl">
+            <div className="block xl:flex px-5 py-10 sm:p-20 xl:space-x-7 space-y-7 xl:space-y-0">
+                <div className="w-full xl:w-2/5 h-96 overflow-y-auto border border-gray-300 shadow rounded-3xl">
                     {applicants.map((applicant, index) => (
                         <>
                             <div className={`hover:bg-gray-50 p-5 space-x-3 w-full cursor-pointer flex items-center`} onClick={() => setSelectedApplicant(index)}>
-                                <img src={applicant.image} className="w-24 h-24 object-cover border border-gray-500 shadow rounded-full" alt={applicant.name} />
+                                <img src={applicant.image} className="sm:w-24 sm:h-24 w-16 h-16 object-cover border border-gray-500 shadow rounded-full" alt={applicant.name} />
                                 <div className="flex w-full justify-between items-center">
                                     <div className="space-y-2">
                                         <h5 className="text-lg">{applicant.name}</h5>
@@ -65,11 +76,11 @@ function Applicants() {
                         </>
                     ))}
                 </div>
-                {!isNaN(selectedApplicant) && <div className="h-full w-3/4 border border-gray-300 shadow rounded-3xl">
+                {!isNaN(selectedApplicant) && <div className="h-full xl:w-3/4 w-full border border-gray-300 shadow rounded-3xl">
                     <div className={`p-5 flex space-x-7 items-center ${applicants[selectedApplicant].approval_status === 1 ? 'bg-green-100' : applicants[selectedApplicant].approval_status === 0 ? 'bg-red-100' : ''} rounded-t-3xl`}>
-                        <img src={applicants[selectedApplicant].image} className="w-24 border rounded-full h-24 object-cover border-gray-500 shadow" alt={applicants[selectedApplicant].name} />
-                        <div className="flex w-full items-center justify-between">
-                            <h5 className="text-2xl">{applicants[selectedApplicant].name}</h5>
+                        <img src={applicants[selectedApplicant].image} className="sm:w-24 sm:h-24 w-16 h-16 border rounded-full object-cover border-gray-500 shadow" alt={applicants[selectedApplicant].name} />
+                        <div className="flex sm:flex-row flex-col w-full items-center justify-between">
+                            <h5 className="sm:text-2xl text-lg text-center font-bold">{applicants[selectedApplicant].name}</h5>
                             <div className="space-x-3 flex">
                                 <CheckIcon className="w-12 p-2 border border-gray-200 bg-white rounded-full shadow hover:bg-primary hover:text-white cursor-pointer duration-100" title="Approve" onClick={() => handleApprovalStatus(true)} />
                                 <XMarkIcon className="w-12 p-2 border border-gray-200 bg-white rounded-full shadow hover:bg-red-500 hover:text-white cursor-pointer duration-100" title="Deny" onClick={() => handleApprovalStatus(false)} />
@@ -79,14 +90,14 @@ function Applicants() {
                     <hr className="h-px bg-gray-100 border-0 w-full" />
                     <div className="p-5 space-y-5">
                         <h4 className="text-lg font-semibold">About</h4>
-                        <p className="text-sm text-gray-600">
+                        <p className="sm:text-sm text-xs text-gray-600">
                             {applicants[selectedApplicant].bio}
                         </p>
                     </div>
                     <hr className="h-px bg-gray-100 border-0 w-full" />
-                    <div className="flex p-5 space-x-7">
-                        <p><strong className="font-semibold">email</strong>: {applicants[selectedApplicant].email}</p>
-                        <p><strong className="font-semibold">Phone Number</strong>: {applicants[selectedApplicant].phone_number}</p>
+                    <div className="flex flex-col sm:flex-row p-5 sm:space-x-7">
+                        <p className="sm:text-base text-sm"><strong className="font-semibold">email</strong>: {applicants[selectedApplicant].email}</p>
+                        <p className="sm:text-base text-sm"><strong className="font-semibold">Phone Number</strong>: {applicants[selectedApplicant].phone_number}</p>
                     </div>
                     <hr className="h-px bg-gray-100 border-0 w-full" />
                     <div className="p-10">
