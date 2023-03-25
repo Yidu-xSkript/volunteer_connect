b12284215ec2057ccde3fcf22d3e2b8833da2210
DMMutua <lengowriter99@gmail.com>
2023-03-25 12:58:21 +0300

    route to access `status` of all missions associated with a particular `user_id`

--- backend/volcon/missions/Controller.py
+++ backend/volcon/missions/Controller.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from volcon.missions.Model import MissionModel
 from volcon.auth.authorization import check_access
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from models import db, Application, Mission
+
 
 missions_bp = Blueprint('missions_bp', __name__, url_prefix='/api/v1/missions')
 model = MissionModel()
@@ -32,3 +35,20 @@ def createMission(user_id):
 def updateMission(mission_id):
     """Update Mission"""
     return model.Update(mission_id)
+
+@missions_bp.route('/applied', methods=['GET'], strict_slashes=False)
+@jwt_required()
+def get_applied_missions_status():
+    """Returns The Status of all Missions associated with a Particular User_id"""
+    user_id = get_jwt_identity()
+
+    applications = Application.query.filter_by(user_id=user_id).all()
+    mission_data = []
+
+    for app in applications:
+        mission_data.append({
+            'mission_id': app.mission_id,
+            'status': app.status
+        })
+
+    return jsonify(mission_data)
