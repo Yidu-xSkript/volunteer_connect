048ae95d94ee1c6cfea781a5aa77f1ed9b65ed03
Yidu-xSkript <yidu95@gmail.com>
2023-03-25 15:48:51 +0300

    Added a new route - Destroy (Applied appropriate role protection for all routes) | Controller name changed

--- backend/volcon/missions/Controller.py
+++ backend/volcon/missions/Controller.py
@@ -1,30 +1,39 @@
 from flask import Blueprint
 from volcon.missions.Model import MissionModel
-from volcon.auth.authorization import check_access
+from mixin.authorization import check_access
+from flask_jwt_extended import get_current_user, jwt_required
 
-missions_bp = Blueprint('missions_bp', __name__, url_prefix='/api/v1/missions')
+MissionController = Blueprint('MissionController', __name__, url_prefix='/api/v1/missions')
 model = MissionModel()
 
-@missions_bp.route('/', methods=['GET'], strict_slashes=False)
+@MissionController.route('/', methods=['GET'], strict_slashes=False)
+@jwt_required()
 def get_missions():
     """Gets All Missions by Default until Search
     Filters are Applied
     """
     return model.get_all_missions()
 
-@missions_bp.route('/<string:mission_id>', methods=['GET'], strict_slashes=False)
+@MissionController.route('/<string:mission_id>', methods=['GET'], strict_slashes=False)
 def get_mission(mission_id):
     """Getting missions by ID"""
     return model.get_mission_by_id(mission_id)
 
-@missions_bp.route('/create/org/<int:user_id>', methods=['post'], strict_slashes=False)
+@MissionController.route('/create/org', methods=['POST'], strict_slashes=False)
 @check_access(['organization'])
-def createMission(user_id):
+def createMission():
     """Create Mission"""
+    user_id = get_current_user().id
     return model.Create(user_id)
 
-@missions_bp.route('/<int:mission_id>/update', methods=['patch'], strict_slashes=False)
+@MissionController.route('/<int:id>/update', methods=['PATCH'], strict_slashes=False)
 @check_access(['organization'])
-def updateMission(mission_id):
+def updateMission(id):
     """Update Mission"""
-    return model.Update(mission_id)
+    return model.Update(id)
+
+@MissionController.route('/<int:id>/destroy', methods=['DELETE'], strict_slashes=False)
+@check_access(['organization'])
+def destroyMission(id):
+    """Destroy Mission"""
+    return model.Destroy(id)
