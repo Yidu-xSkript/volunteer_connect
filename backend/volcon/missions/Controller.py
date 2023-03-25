09577541c3b88be6c46d496c0c1f71dea42e0d3c
DMMutua <lengowriter99@gmail.com>
2023-03-25 12:14:21 +0300

    patch and put in caps

--- backend/volcon/missions/Controller.py
+++ backend/volcon/missions/Controller.py
@@ -5,6 +5,7 @@ from volcon.auth.authorization import check_access
 missions_bp = Blueprint('missions_bp', __name__, url_prefix='/api/v1/missions')
 model = MissionModel()
 
+
 @missions_bp.route('/', methods=['GET'], strict_slashes=False)
 def get_missions():
     """Gets All Missions by Default until Search
@@ -12,18 +13,21 @@ def get_missions():
     """
     return model.get_all_missions()
 
+
 @missions_bp.route('/<string:mission_id>', methods=['GET'], strict_slashes=False)
 def get_mission(mission_id):
     """Getting missions by ID"""
     return model.get_mission_by_id(mission_id)
 
-@missions_bp.route('/create/org/<int:user_id>', methods=['post'], strict_slashes=False)
+
+@missions_bp.route('/create/org/<int:user_id>', methods=['POST'], strict_slashes=False)
 @check_access(['organization'])
 def createMission(user_id):
     """Create Mission"""
     return model.Create(user_id)
 
-@missions_bp.route('/<int:mission_id>/update', methods=['patch'], strict_slashes=False)
+
+@missions_bp.route('/<int:mission_id>/update', methods=['PATCH'], strict_slashes=False)
 @check_access(['organization'])
 def updateMission(mission_id):
     """Update Mission"""
