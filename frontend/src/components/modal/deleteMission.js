fb43138f6c9d8608a7cd0a3e2a97264cb5e2c47a
Yidu-xSkript <yidu95@gmail.com>
2023-03-29 17:54:18 +0300

    destroy function created - calls to api | populate organization missions & missions in total on front end on response.

--- frontend/src/components/modal/deleteMission.js
+++ frontend/src/components/modal/deleteMission.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import Modal from ".";
+import AxiosService from "../../services/axios.services";
 
-function DeleteMission({ showModal, close }) {
+function DeleteMission({ showModal, close, mission, setMissions, setMissions_ }) {
+    const [isLoading, setIsLoading] = useState()
+    const { _api } = AxiosService()
+    const destroy = () => {
+        setIsLoading(true)
+        _api.delete(`/missions/${mission.id}/destroy`).then(res => {
+            setIsLoading(false)
+            setMissions(JSON.stringify(res.data.missions))
+            setMissions_([...res.data.org_missions])
+        }).catch(err => {
+            setIsLoading(false)
+            console.log(err)
+        })
+    }
     return (
-        <Modal showModal={showModal} close={close} buttonName={"Delete Mission"} title={"Delete Mission"}>
+        <Modal onClick={destroy} isLoading={isLoading} showModal={showModal} close={close} buttonName={"Delete Mission"} title={"Delete Mission"}>
             <div className="flex items-center justify-center">
                 <p className="text-2xl">Are you sure?</p>
             </div>
