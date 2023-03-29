b652ba11382d0ffeed9248b821227bd93cbc83ab
Yidu-xSkript <yidu95@gmail.com>
2023-03-29 18:20:56 +0300

    called useMissions hook to update localstorage | used useEffect hook to call missions and populate the page | updated Modals (added checks to see if 'missions_' array state has any data.) | Pagination design removed - we have no time left.

--- frontend/src/pages/missions/index.js
+++ frontend/src/pages/missions/index.js
@@ -7,93 +7,28 @@ import { useEffect, useState } from "react";
 import ManageMissionModal from "../../components/modal/manageMission";
 import DeleteMissionModal from "../../components/modal/deleteMission";
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
-import axios from "axios";
+import AxiosService from "../../services/axios.services";
+import useMission from '../../hooks/useMission';
 
 function Mission() {
-    const missions = [
-        {
-            id: 1,
-            applicants: 5,
-            deadline: 'Feb 02, 23',
-            est_time: '1 to 3 months',
-            post_human_date: '2 hours ago',
-            title: 'Mission Title 1',
-            volunteering_hours: 5,
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        },
-        {
-            id: 2,
-            applicants: 5,
-            deadline: 'Feb 02, 23',
-            est_time: '1 to 3 months',
-            post_human_date: '2 hours ago',
-            title: 'Mission Title 1',
-            volunteering_hours: 5,
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        },
-        {
-            id: 3,
-            applicants: 5,
-            deadline: 'Feb 02, 23',
-            est_time: '1 to 3 months',
-            post_human_date: '2 hours ago',
-            title: 'Mission Title 1',
-            volunteering_hours: 5,
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        },
-        {
-            id: 4,
-            applicants: 5,
-            deadline: 'Feb 02, 23',
-            est_time: '1 to 3 months',
-            post_human_date: '2 hours ago',
-            title: 'Mission Title 1',
-            volunteering_hours: 5,
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        },
-        {
-            id: 5,
-            applicants: 5,
-            deadline: 'Feb 02, 23',
-            est_time: '1 to 3 months',
-            post_human_date: '2 hours ago',
-            title: 'Mission Title 1',
-            volunteering_hours: 5,
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        },
-        {
-            id: 6,
-            applicants: 5,
-            deadline: 'Feb 02, 23',
-            est_time: '1 to 3 months',
-            post_human_date: '2 hours ago',
-            title: 'Mission Title 1',
-            volunteering_hours: 5,
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        }
-    ]
+    const [missions_, setMissions_] = useState([])
+    const { setMissions } = useMission()
 
     const [showModal, setShowModal] = useState(false)
     const [showDelModal, setShowDelModal] = useState(false)
     const [type, setType] = useState()
     const [selectedMission, setSelectedMission] = useState()
-
-    const getMissions = () => {
-        // axios api goes here.
-        axios.get(process.env.API_DEV_URL+'/').then(res => (
-            console.log(res.data)
-        )).catch(err => console.log(err))
-    }
+    const { _api } = AxiosService()
 
     const bodyScrollStatus = (disable = true) => {
         const targetElement = document.querySelector('#modal');
         disable ? disableBodyScroll(targetElement) : enableBodyScroll(targetElement);
     }
-    const openEditModal = (id) => {
-        setSelectedMission(id)
+    const openEditModal = (index) => {
+        setSelectedMission(index)
+        bodyScrollStatus()
         setType("Edit")
         setShowModal(true)
-        bodyScrollStatus()
     }
     const openAddModal = () => {
         setType("Add")
@@ -103,45 +38,43 @@ function Mission() {
     const closeModal = () => {
         if (showModal) setShowModal(false)
         if (showDelModal) setShowDelModal(false)
+        setSelectedMission()
         bodyScrollStatus(false)
+        setType('')
     }
-    const OpenDeleteModal = () => {
+    const OpenDeleteModal = (index) => {
+        setSelectedMission(index)
         setShowDelModal(true)
         bodyScrollStatus()
     }
 
     useEffect(() => {
-        if (type === 'Edit') getMissions()
-    }, [type])
+        if (missions_.length === 0)
+            _api.get(`/missions/organization`).then(res => {
+                setMissions_([...res.data])
+            }).catch(err => console.log(err))
+    }, [missions_, _api])
 
     return (
         <>
             {/* Add Modal + Edit Modal  - defined with type */}
-            <ManageMissionModal showModal={showModal} type={type} close={closeModal} mission={missions[selectedMission]} />
-            <DeleteMissionModal showModal={showDelModal} close={closeModal} />
+            {missions_.length > 0 & selectedMission !== undefined || type === 'Add' ? <ManageMissionModal setMissions={setMissions} setMissions_={setMissions_} showModal={showModal} type={type} close={closeModal} mission={missions_[selectedMission]} /> : ''}
+            {missions_.length > 0 && selectedMission !== undefined && <DeleteMissionModal setMissions={setMissions} setMissions_={setMissions_} mission={missions_[selectedMission]} showModal={showDelModal} close={closeModal} />}
             <AuthLayout>
                 <div className="relative">
                     <div className="p-10 mx-auto container">
-                        {missions.length > 0 && <div className="flex justify-center my-10">
+                        {missions_.length > 0 && <div className="flex justify-center my-10">
                             <h1 className="text-4xl uppercase tracking-[0.3rem] font-light">Missions</h1>
                         </div>}
                         {/* Checks if there are any missions created */}
-                        {missions.length > 0 ?
+                        {missions_.length > 0 ?
                             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-7 my-10">
-                                {missions.map((mission) => (
-                                    // I'm referring to the attributes 1by1 for ease. I'm gonna pass the whole object later when i send a get request to the api.
+                                {missions_.map((mission, index) => (
                                     <OrgMissionCard
-                                        key={mission.id}
-                                        applicants={mission.applicants}
-                                        deadline={mission.deadline}
-                                        description={mission.description}
-                                        est_time={mission.est_time}
-                                        id={mission.id}
-                                        post_human_date={mission.post_human_date}
-                                        title={mission.title}
-                                        volunteering_hours={mission.volunteering_hours}
-                                        openEditModal={openEditModal}
-                                        openDeleteModal={OpenDeleteModal} />
+                                        key={index}
+                                        mission={mission}
+                                        openEditModal={() => openEditModal(index)}
+                                        openDeleteModal={() => OpenDeleteModal(index)} />
                                 ))}
                             </div> :
                             <div className="rounded-3xl border border-gray-300 shadow p-10 grid grid-cols-4">
@@ -152,7 +85,8 @@ function Mission() {
                                     <Button text={"Create Mission"} className="text-xl px-8 rounded-full" action={openAddModal} />
                                 </div>
                             </div>}
-                        {missions.length > 0 && <div className="cursor-pointer text-lg text-center uppercase tracking-[0.2rem] font-semibold" title="Load More Missions">Load More</div>}
+                        {/* I'm not going to do pagination because i don't have time. */}
+                        {/* {missions_.length > 0 && <div className="cursor-pointer text-lg text-center uppercase tracking-[0.2rem] font-semibold" title="Load More Missions">Load More</div>} */}
                     </div>
                     <PlusIcon className="w-20 p-3 rounded-full bg-primary shadow-xl fixed bottom-10 right-10 text-white cursor-pointer" title="Add Mission" onClick={openAddModal} />
                 </div>
