6c8ae62aa4fda50a2aa14197bd21869404a3eff6
Yidu-xSkript <yidu95@gmail.com>
2023-03-29 18:09:01 +0300

    removeMissions hook called on 401 error response

--- frontend/src/services/axios.services.js
+++ frontend/src/services/axios.services.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useNavigate } from "react-router";
+import useMission from "../hooks/useMission";
 import useToken from "../hooks/useToken";
 import useUser from "../hooks/useUser";
 import API from "../utils/API";
@@ -8,6 +9,7 @@ function AxiosService() {
     const { base_api_url } = API()
     const { token, removeToken, setToken } = useToken()
     const { user, removeUser } = useUser()
+    const { missions, removeMissions } = useMission()
     const navigate = useNavigate()
 
     const _api = axios.create({
@@ -19,7 +21,6 @@ function AxiosService() {
         (response) => {
             const _token = response.data['token']
             if (_token) {
-                if (token) removeToken()
                 setToken(_token)
             }
             return Promise.resolve(response)
@@ -29,6 +30,7 @@ function AxiosService() {
                 console.error("You are not logged in!");
                 if (token) removeToken()
                 if (user) removeUser()
+                if (missions) removeMissions()
                 navigate('/login')
                 return Promise.reject();
             }
