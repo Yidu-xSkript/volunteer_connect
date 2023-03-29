62095a9f70ceef316d9c1cadef4a4e0075f2e004
Yidu-xSkript <yidu95@gmail.com>
2023-03-29 16:57:25 +0300

    Create/Apply method - added checks to see if a new resume was uploaded & returned user info instead of application row | updated getAllForUser method - removed @staticmethod decoration to call the method inside of other methods & removed user_id argument to call the current_user from within the method instead of the controller & removed aliased argument on join statement | updated 'Update' method - returns getAllForUser method & removed @staticmethod decoration. | updated Destroy method to account for relationship with missions. -- deletes all applications based on the mission_id arg. | Updated applications controller -- removed user.id invocation from route('/')

--- backend/volcon/applications/Model.py
+++ backend/volcon/applications/Model.py
@@ -3,33 +3,39 @@
 
 from flask import jsonify, request
 from sqlalchemy.exc import SQLAlchemyError
-from models.volcon_db import db, Application
-
+from models.volcon_db import db, Application, Volunteer, User
+from flask_jwt_extended import get_current_user
 
 class ApplicationModel:
     @staticmethod
     def Create(user_id, mission_id):
         try:
+            data = request.get_json()
+            volunteer: Volunteer = Volunteer.query.get(user_id)
+            if volunteer.resume is None and data.get("resume"):
+                volunteer.updateResume(user_id, data.get("resume", volunteer.resume))
+            if volunteer.resume is None:
+                return jsonify({'error': 'Resume Not Found! Please add your resume!'}), 404
             application = Application(
                 user_id=user_id,
                 mission_id=mission_id
             )
             db.session.add(application)
             db.session.commit()
-            return application.to_dict()
+            return volunteer.to_dict()
         except SQLAlchemyError as e:
             error = str(e.__dict__.get('orig', e))
             return jsonify({'error': error}), 500
 
-    @staticmethod
-    def getAllForUser(user_id, role):
+    def getAllForUser(self, role):
         try:
+            user: User = get_current_user()
             if role == 'volunteer':
                 applications: Application = Application.query.filter_by(
-                    user_id=user_id).all()
+                    user_id=user.id).all()
             if role == 'organization':
                 applications: Application = Application.query.join(
-                    Application.mission, aliased=True).filter_by(org_id=user_id).all()
+                    Application.mission).filter_by(org_id=user.id).all()
             return [application.to_dict() for application in applications]
         except SQLAlchemyError as e:
             error = str(e.__dict__.get('orig', e))
@@ -47,31 +53,27 @@ class ApplicationModel:
     #         error = str(e.__dict__.get('orig', e))
     #         return jsonify({'error': error}), 500
 
-    @staticmethod
-    def Update(id):
+    def Update(self, id):
         try:
             data = request.get_json()
             application: Application = Application.query.get(id)
             if application:
                 application.status = data.get('status', application.status)
                 db.session.commit()
-                return application.to_dict()
+                return self.getAllForUser("organization")
             else:
                 return jsonify({'error': 'Application not found'}), 404
         except SQLAlchemyError as e:
             error = str(e.__dict__.get('orig', e))
             return jsonify({'error': error}), 500
 
-    # @staticmethod
-    # def Destroy(application_id):
-    #     try:
-    #         application = Application.query.get(application_id)
-    #         if application:
-    #             db.session.delete(application)
-    #             db.session.commit()
-    #             return jsonify({'message': 'Application deleted successfully'})
-    #         else:
-    #             return jsonify({'error': 'Application not found'}), 404
-    #     except SQLAlchemyError as e:
-    #         error = str(e.__dict__.get('orig', e))
-    #         return jsonify({'error': error}), 500
+    def Destroy(self, mission_id):
+        """Destroy all applications based mission r/n ship"""
+        try:
+            apps = Application.__table__.delete().where(Application.mission_id == mission_id)
+            db.session.execute(apps)
+            db.session.commit()
+            return
+        except SQLAlchemyError as e:
+            error = str(e.__dict__.get('orig', e))
+            return error, 500
